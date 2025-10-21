c31fbf7f30ba4e61e61ba27b85b3a1ef434adfbb
DESKTOP-KTEOBUD\GA2A <h1202419013@hiratagakuen.onmicrosoft.com>
2025-10-21 16:34:38 +0900

    ボタンをクリックしたらごはんが出てくるのをどうにかしたい

--- Assets/Script/Cat_cat.cs
+++ Assets/Script/Cat_cat.cs
@@ -11,7 +11,7 @@ public class Cat_cat : MonoBehaviour
     [SerializeField] GameObject scottish;  // スコティッシュ
     [SerializeField] GameObject black;     // 黒ネコ
     [SerializeField] GameObject white;     // 白ネコ
-    [SerializeField] GameObject sabatora;      // サバトラ
+    [SerializeField] GameObject sabatora;  // サバトラ
     [SerializeField] GameObject cyatora;   // 茶トラ
 
     [SerializeField] GameObject[] Cat = new GameObject[14];
--- Assets/Script/meal_fall.cs
+++ Assets/Script/meal_fall.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class meal_fall : MonoBehaviour
@@ -14,15 +15,42 @@ public class meal_fall : MonoBehaviour
 
     void Update()
     {
+        OnMouseOver();
+    }
+
+    private void OnMouseOver()
+    {
+        /*
+        ボタンを押したときもクリックしてしまう判定になるため、
+        「ボタンの上にカーソルが乗っている時はクリックの判定を取らない」をしたい！
+
+        EventTrrigerだとか座標だとかあるけど、今回はRayを飛ばして判定する！
+        */
 
         // 左クリックしたとき
         if (Input.GetMouseButton(0))
         {
-            Debug.Log("左クリックした");
-            // 一定の時間が経過するごとに
-            if (Time.frameCount % 200 == 0)
-            {
-                Instantiate(Meal_prefab, feedPoinit, Quaternion.identity);
+            // Rayを飛ばす
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D raycastHit2D = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            /*
+            RaycastHit2D raycastHit2D…レイが何かに当たった場合の
+                                       情報（位置や当たったオブジェクトなど）を格納する変数。
+            
+            Physics2D.Raycast()…Unityの2D物理エンジンで
+                                 レイキャストを行う関数。
+
+            ～引数～
+                (Vector2)ray.origin…レイの開始位置。
+                (Vector2)ray.direction…レイの進む方向。
+             */
+
+            if (!raycastHit2D) { // Rayで何もヒットしなかったら画面クリックと考える
+                Debug.Log("左クリックした");
+                // 一定の時間が経過するごとに
+                if (Time.frameCount % 200 == 0)
+                    Instantiate(Meal_prefab, feedPoinit, Quaternion.identity);
             }
         }
     }
